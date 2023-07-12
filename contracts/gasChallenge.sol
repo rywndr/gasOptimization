eea168809ec1797d05cb8c0824ce6856bdb5449c
Haikhal Roywendra <88234565+rywndr@users.noreply.github.com>
2023-07-12 14:04:21 +0700

    Add files via upload

---
+++ contracts/gasChallenge.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract gasChallenge {
+    //Implement Fixed-Size Array Technique Here
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
+    
+    //Implement Caching State Variable Technique Here
+    function getSumOfArray() public view returns (uint256) {
+        uint sum = 0;
+        for (uint i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    
+    function notOptimizedFunction() public {
+        for (uint i = 0; i < numbers.length; i++) {
+            numbers[i] = 0;
+        }
+    }
+    
+    //Implement Remaining Gas Optimization Techniques Here
+    //Sum of elements in the numbers array should equal 0
+    function optimizedFunction() public {
+        uint sum = getSumOfArray();
+        for (uint i = 0; i < 10; i++) {
+            unchecked {
+                numbers[i] = 0;
+            }
+        }
+    }
+}
